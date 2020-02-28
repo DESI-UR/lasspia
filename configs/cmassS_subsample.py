1a0a92e0b113e07a61fc190ef5f5201f746186e2
Zack Brown <39375839+zbrown89@users.noreply.github.com>
2020-02-28 11:38:14 -0500

    Delete cmassS_subsample.py

--- configs/cmassS_subsample.py
+++
@@ -1,5 +0,0 @@
-from cmassS import cmassS
-
-class cmassS_subsample(cmassS):
-    def binningRA(self): return {"bins": 220, "range":(0,10)}
-    def binningDec(self): return {"bins":156, "range":(0,6)}
